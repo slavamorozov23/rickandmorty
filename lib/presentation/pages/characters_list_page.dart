c4c4e1a57a402d98424f7a710fc1b2e793f00ee6
Slava Morozov <slavamorozov20052305@gmail.com>
2024-09-01 19:25:50 +0500

    some changes

--- lib/presentation/pages/characters_list_page.dart
+++ lib/presentation/pages/characters_list_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../../domain/domain.dart';
 import '../cubit/characters_list/characters_list_cubit.dart';
 import '../widgets/lazy_loading_list.dart';
